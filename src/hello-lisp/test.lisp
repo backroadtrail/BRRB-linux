7fcd44251febff0aa57a72631231441963d50456
donaldranderson <2606216+donaldranderson@users.noreply.github.com>
2020-11-23 22:41:39 -0500

    Create test.lisp

---
+++ src/hello-lisp/test.lisp
@@ -0,0 +1,22 @@
+#!/usr/bin/env sbcl --script
+;;;; test.lisp
+
+;;;; Copyright 2020 OpsResearch LLC
+;;;;
+;;;; This file is part of Backroad Raspberry.
+;;;;
+;;;; Backroad Raspberry is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU Affero General Public License as published by
+;;;; the Free Software Foundation, either version 3 of the License, or
+;;;; (at your option) any later version.
+;;;;
+;;;; Backroad Raspberry is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU Affero General Public License
+;;;; along with Backroad Raspberry.  If not, see <https://www.gnu.org/licenses/>.
+
+ (ql:quickload "hello-lisp")
+ (hello-lisp:hello-world)
